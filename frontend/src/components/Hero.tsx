9d94b0df734843dee96b6056076c0ee6d0cde7df
Muhammed Zain <muhammedzain2256@gmail.com>
2024-02-21 15:10:25 +0500

    Frontend Changes

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <div className="bg-blue-800 pb-16">
+      <div className="container flex flex-col gap-2">
+        <h1 className="text-2xl text-white font-bold  md:text-5xl">
+          Find your next stay with us
+        </h1>
+        <p className="text-xl md:text-2xl text-white  font-semibold">
+          Search low Prices on Hotels
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
