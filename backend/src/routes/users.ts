400d9f82b0761b21858fcf9369363ad6452f15a8
Muhammed Zain <muhammedzain2256@gmail.com>
2024-02-24 09:50:06 +0500

    Users

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,22 +1,27 @@
-import express, {Request, Response} from 'express';
-import User from '../models/user';
-
+import express, { Request, Response } from "express";
+import User from "../models/user";
+import jwt from "jsonwebtoken";
 const router = express.Router();
 
-router.post("/register", async (req:Request, res:Response) => {
-    try {
-        let user = await User.findOne({
-        email: req.body.email,
+router.post("/register", async (req: Request, res: Response) => {
+  try {
+    let user = await User.findOne({
+      email: req.body.email,
     });
 
     if (user) {
-        return res.status(400).json({message: "User Already Exists..."})
+      return res.status(400).json({ message: "User Already Exists..." });
     }
 
     user = new User(req.body);
     await user.save();
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: "Something went wrong" })
-    }
+
+    const token = jwt.sign(
+        { userId: user.id },
+        process.env.JWT_SECRET_KEY as string
+      );
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Something went wrong" });
+  }
 });
