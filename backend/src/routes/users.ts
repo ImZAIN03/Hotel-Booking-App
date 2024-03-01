1a9afcd0f6e3dd977970a7330c1f84ee7ab59eed
Muhammed Zain <muhammedzain2256@gmail.com>
2024-03-01 10:40:46 +0500

    Backend

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express";
 import User from "../models/user";
 import jwt from "jsonwebtoken";
+
 const router = express.Router();
 
+//
 router.post("/register", async (req: Request, res: Response) => {
   try {
     let user = await User.findOne({
@@ -17,11 +19,23 @@ router.post("/register", async (req: Request, res: Response) => {
     await user.save();
 
     const token = jwt.sign(
-        { userId: user.id },
-        process.env.JWT_SECRET_KEY as string
-      );
+      { userId: user.id },
+      process.env.JWT_SECRET_KEY as string,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    res.cookie("auth token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 86400000, 
+    })
+    return res.sendStatus(200);
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: "Something went wrong" });
   }
 });
+
+export default router
